600a4ec31087d0047f4fb7592b05f148ec203fbd
cockochan <56898295+cockochan@users.noreply.github.com>
2020-10-17 22:27:23 +0300

    third

--- server.js
+++ server.js
@@ -5,5 +5,12 @@ const connectDB = require("./config/db")
 connectDB();
 
 app.get('/', (req,res)=> res.send('API Running'))
+
+//Define Routes
+app.use('.api/users', require('./routes/api/users'));
+app.use('.api/auth', require('./routes/api/auth'));
+app.use('.api/posts', require('./routes/api/posts'));
+app.use('.api/profile', require('./routes/api/profile'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>console.log(`Server started on port ${PORT}`));
