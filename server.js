6c4a950d1bf9e6800fdcf0bd6acc6e488f5f84c3
cockochan <56898295+cockochan@users.noreply.github.com>
2020-10-17 22:01:11 +0300

    Second commit

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./config/db")
+//connect database
+connectDB();
+
 app.get('/', (req,res)=> res.send('API Running'))
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>console.log(`Server started on port ${PORT}`));
