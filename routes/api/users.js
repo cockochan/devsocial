e39c4f334e2135be96b89cc7cbe9a79dc3d72a4c
cockochan <56898295+cockochan@users.noreply.github.com>
2020-10-18 01:23:09 +0300

    go sleep a bit

--- routes/api/users.js
+++ routes/api/users.js
@@ -29,7 +29,7 @@ router.post(
     try {
       let user = await User.findOne({ email });
       if (user) {
-        res.status(400).json({ errors: [{ msg: "User already exists" }] });
+       return res.status(400).json({ errors: [{ msg: "User already exists" }] });
       }
 
       const avatar = gravatar.url(email, {
