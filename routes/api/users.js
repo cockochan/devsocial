81912e0c16d19747ff840f7be2a110eb386626cd
cockochan <56898295+cockochan@users.noreply.github.com>
2020-10-18 11:35:53 +0300

    token

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const gravatar = require("gravatar");
 const User = require("../../models/User");
 const bcrypt = require("bcryptjs");
+const config = require('config')
 //@route  POST api/users
 //@desc   register user
 //@access Public
@@ -47,7 +48,28 @@ router.post(
 
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
+
       await user.save();
+
+      const payload = {
+        user:{
+          id:user.id
+
+        }
+      }
+
+      jwt.sign(
+        payload, 
+        config.get('jwtToken'),
+        {expiresIn:360000},
+        (err, token) => {
+          if(err) throw err;
+          res.json({token});
+
+          
+        }
+        )
+
       //Return jsonwebtoken
 
       res.send("User registered");
