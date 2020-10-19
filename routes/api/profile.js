ec1df7ba57df20ec04a110abd65674a17111631a
cockochan <56898295+cockochan@users.noreply.github.com>
2020-10-19 23:14:26 +0300

    add ex to profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -143,4 +143,50 @@ router.delete("/", auth, async (req, res) => {
     res.status(500).send({ msg: "Server Error" });
   }
 });
+
+//@route  PUT api/profile/experience
+//@desc   Add profile experience
+//@access Private
+
+router.put('/experience',[auth,[
+    check('title','Title is required').not().isEmpty(),
+    check('company','Company is required').not().isEmpty(),
+    check('from','From date is required').not().isEmpty(),
+]], async (req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return status(400).json({ errors:errors.array() });
+    }
+    const{
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    }=req.body;
+    const newExp = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    }
+    try {
+        const profile = await Profile.findOne({ user:req.user.id })
+        profile.experience.unshift(newExp)
+
+        await profile.save()
+
+        res.json(profile)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+
+})
+
 module.exports = router;
