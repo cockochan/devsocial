e21fb4602f5b7c21e4c28fee070f544c6699fe43
cockochan <56898295+cockochan@users.noreply.github.com>
2020-10-19 22:16:29 +0300

    1

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -103,4 +103,25 @@ router.get('/', async (req, res)=>{
 
 )
 
+//@route  GET api/profile/user:user._id
+//@desc   GET profile by user id
+//@access Public
+router.get('/user/:user_id', async (req, res)=>{
+
+    try{
+        const profile = await Profile.findOne({user:req.params.user_id}).populate('user',['name','avatar'])
+        if(!profile) return res.status(400).json({msg:'Profile not found'})
+        res.json(profile)
+    }catch(err){
+        console.error(err.message)
+        if(err.kind=='ObjectId'){
+            return res.status(400).json({msg:'Profile not found'})
+        }
+        res.status(500).send({msg:'Server Error'})
+    }
+}
+
+)
+
+
 module.exports = router;
