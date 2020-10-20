e9f870c300e826ca9ea91259986f473745e2ce7c
cockochan <56898295+cockochan@users.noreply.github.com>
2020-10-21 02:59:43 +0300

    brought Html

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React,{ Fragment } from 'react';
+import Navbar from "../src/layout/Navbar"
+import Landing from "../src/layout/Landing"
+import './App.css';
+
+const App = () => 
+
+    <Fragment>
+<Navbar/>
+<Landing />
+    </Fragment>
+
+
+export default App
