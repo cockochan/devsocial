f07960529ae67ac69e8c95eb8971337cacea59d3
cockochan <56898295+cockochan@users.noreply.github.com>
2020-10-20 22:23:27 +0300

    one can post comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -146,5 +146,34 @@ await post.save();
         res.status(500).send('Server Error');
     }
 })
+//@route  POST api/posts/comment/:id
+//@desc   Comment on a post
+//@access Private
+router.post(
+    "/comment/:id",
+    [auth, [check("text", "Text is required").not().isEmpty()]],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors:errors.array()})
+        }
+  try {
+      const user = await User.findByIdAndUpdate(req.user.id).select('-password');
+    const post =await Post.findById(req.params.id)
 
+      const newComment = ({
+          text:req.body.text,
+          name: user.name,
+          avatar: user.avatar,
+          user:req.user.id
+      })
+      post.comments.unshift(newComment)
+  await post.save();
+      res.json(post.comments)
+  } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error'); 
+  }
+    }
+  );
 module.exports = router;
