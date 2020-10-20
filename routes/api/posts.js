b90f5f4cf215c7bf116c391ea005efdb7ab3a572
cockochan <56898295+cockochan@users.noreply.github.com>
2020-10-20 14:17:21 +0300

    can get posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -30,14 +30,23 @@ try {
     res.json(post)
 } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error');
-
-    
+    res.status(500).send('Server Error'); 
 }
-     
-
-   
   }
 );
+//@route  GET api/posts
+//@desc   GET all posts
+//@access Private
+
+router.get('/', auth, async(req, res)=>{
+    try {
+        const posts =await Post.find().sort({date:-1});
+        res.json(posts)
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error'); 
+    }
+})
+
 
 module.exports = router;
