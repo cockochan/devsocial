311affa740d415d9472418b047d86c80ef851780
cockochan <56898295+cockochan@users.noreply.github.com>
2020-10-22 03:09:25 +0300

    setAlert

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react'
-import axios from 'axios'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+import {setAlert} from '../../actions/alert'
+import PropTypes from 'prop-types'
 
-export default function Register() {
+ const Register=({setAlert})=> {
     const [formData, setFormData]=useState({
 
         name:'',
@@ -15,8 +18,10 @@ const onChange = e => setFormData({...formData, [e.target.name]:e.target.value})
 const onSubmit = async e =>{
    e.preventDefault();
 if(password!==password2){
-  console.log("Passwords do not match")
+  setAlert("Passwords do not match",'danger');
+  
 }else{
+
 
    console.log('success')
  
@@ -61,8 +66,14 @@ if(password!==password2){
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="login.html">Sign In</Link>
       </p>
         </Fragment>
-    )
+    );
+  
+  } 
+
+Register.propTypes = {
+  setAlert:PropTypes.func.isRequired
 }
+export default connect(null,{setAlert})(Register)
---
+++ client/src/layout/Alert.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Alert(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Alert.propTypes = {
+
+}
+
+export default Alert
+
