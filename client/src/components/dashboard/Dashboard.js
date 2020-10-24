d781e6104411c64466df4090f70baa48cabf9f7a
cockochan <56898295+cockochan@users.noreply.github.com>
2020-10-25 00:59:33 +0300

    get current profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React,{useEffect} from 'react';
+import {connect}from 'react-redux'
 import PropTypes from 'prop-types'
-
-const Dashboard = props => {
+import{ getCurrentProfile} from '../../actions/profile'
+const Dashboard = ({getCurrentProfile, auth, profile})=> {
+    useEffect(()=>{
+        getCurrentProfile();
+    },[])
     return (
         <div>
             Dashboard
         </div>
     )
 }
-
-Dashboard.propTypes = {
-
+Dashboard.propTypes ={
+    getCurrentProfile: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    profile:PropTypes.object.isRequired
 }
+const mapStateToProps =state =>({
+    auth:state.auth,
+    profile:state.profile
+})
 
-export default Dashboard
+
+export default connect (mapStateToProps,{getCurrentProfile})(Dashboard)
