d7351c20ebb1920e3af04be591b8434abc9bd959
Constantin Dimitrenco <56898295+cockochan@users.noreply.github.com>
2020-11-21 10:33:30 +0000

    bug

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -91,17 +91,16 @@ export const getProfileById =userId=>async dispatch=>{
 
     //get  github repos
 
-export const getGithubRepos =username=>async dispatch=>{
 
-    try{
-    axios.defaults.headers.common['x-auth-token'] = localStorage.token;
-
-    const res = await axios.get(`/api/profile/github/${username}`)
-    dispatch({
-        type:GET_REPOS,
-        payload:res.data
-    })
-  } catch (err) {
+    export const getGithubRepos = (username) => async (dispatch) => {
+        try {
+          const res = await axios.get(`/profile/github/${username}`);
+      
+          dispatch({
+            type: GET_REPOS,
+            payload: res.data
+          });
+        }catch (err) {
      
       dispatch({
           type:PROFILE_ERROR,
