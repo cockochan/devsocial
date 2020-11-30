c5b24e1ca8a4828a6c1c2569ef1cad2c99766fd6
Constantin Dimitrenco <56898295+cockochan@users.noreply.github.com>
2020-11-30 23:03:33 +0000

    production to heroku

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db")
+const path = require('path')
 
 //connect database
 connectDB();
 
 //init middleware
 app.use(express.json({extended:false}))
-app.get('/', (req,res)=> res.send('API Running'))
 
 
 
@@ -16,6 +16,18 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
+
+//Serve static assets in production
+
+if(orocess.env.NODE_ENV==='production'){
+//Set static folder
+app.use(express.static('client/build'));
+
+app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(_dirname, 'client', 'build','index.html'))
+});
+
+}
 
 
 const PORT = process.env.PORT || 5000;
