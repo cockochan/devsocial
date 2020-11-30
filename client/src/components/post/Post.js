594c734c1da762361315460194f85b866f4f319f
Constantin Dimitrenco <56898295+cockochan@users.noreply.github.com>
2020-11-30 14:31:52 +0000

    sdfasdf]#

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,6 +4,7 @@ import Spinner from '../../layout/Spinner'
 import {getPost} from '../../actions/post'
 import PropTypes from 'prop-types'
 import PostItem from "../posts/PostItem"
+import CommentItem from "../posts/CommentItem"
 import CommentForm from "../post/CommentForm"
 const Post = ({getPost,post:{post, loading}, match}) => {
     useEffect(()=>{
@@ -13,6 +14,11 @@ const Post = ({getPost,post:{post, loading}, match}) => {
     return loading || post === null?<Spinner/>:<Fragment>
         <PostItem post={post}showActions={false}></PostItem>
         <CommentForm postId={post._id}/>
+        <div className="comments">
+        {post.comments.map((comment) => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </Fragment>
 }
 
